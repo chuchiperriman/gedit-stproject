35fd26f2357bc7fb52b2670f84b1f7cc57ef4ace
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2013-03-30 13:35:49 +0100

    Save recent projects

--- stproject/config.py
+++ stproject/config.py
@@ -21,15 +21,31 @@ def _load():
                 _config = json.load(fp)
         except:
             _config = {}
+            
+        if not 'recent_projects' in _config:
+            _config['recent_projects'] = []
+    
+def _add_recent_project(path):
+    global _config, _config_file
+        
+    for p in _config['recent_projects']:
+        if p == path:
+            _config['recent_projects'].remove(p)
+            
+    _config['recent_projects'].insert(0, path)
+    _config['recent_projects'] = _config['recent_projects'][-5:]
     
 def save_last_project(path):
     global _config, _config_file
     _load()
         
     _config['last_open'] = path
+    
+    _add_recent_project(path)
+    
     with open(_config_file, 'w') as fp:
         json.dump(_config, fp, indent=4)
-            
+
 def get_last_project():
     _load()
         
